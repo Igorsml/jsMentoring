025b360cbd8a6d17b13cd9e12561405ba93965e0
Igorsml <igorsmlff@gmail.com>
2022-06-15 00:16:37 +0300

    feat: debugger

---
+++ learnJS/Owe/08.06.2022/debugger/debugger.js
@@ -0,0 +1,48 @@
+// fix scrypt in head
+document.addEventListener('DOMContentLoaded', () => {
+  const num1 = document.querySelector('#num1')
+  const num2 = document.querySelector('#num2')
+  const addBtn = document.querySelector('#add')
+  const subBtn = document.querySelector('#sub')
+  const output = document.querySelector('#output')
+
+  function getInputValues() {
+    const value1 = +num1.value;
+    const value2 = parseInt(num2.value);
+
+    return [value1, value2]
+  }
+
+  function addHandler() {
+    const values = getInputValues()
+    console.log('values:', values) // string in arr
+
+    const result = values[0] + values[1]
+    console.log('result:', typeof (result)) // string
+    displayResult(result)
+  }
+
+  function subHandler() {
+    const values = getInputValues()
+
+    const result = values[0] - values[1]
+    displayResult(result)
+  }
+
+  function displayResult(result) {
+    output.closest('.card').style.display = 'block'
+    output.innerHTML = `Result = ${result}`
+  }
+
+  // console.log('null?', addBtn) // null
+  addBtn.addEventListener('click', addHandler)
+  subBtn.addEventListener('click', subHandler)
+
+})
+
+/*
+1) We can hover on values, methods
+2) Escape in source open console 
+3) Deactivate breakpoints
+4) Watch values 
+*/
---
+++ learnJS/part 1/chap_4/4.7_symbol.js
@@ -0,0 +1,5 @@
+'use strict'
+
+let id = Symbol('id'); // let's create symbol, id - name of symbol
+
+let symbol
