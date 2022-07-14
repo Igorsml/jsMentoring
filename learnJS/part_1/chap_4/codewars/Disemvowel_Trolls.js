3c78b37b4759c7f4942e3147a9b3e3c763bf43cb
Igor <75610081+Igorsml@users.noreply.github.com>
2022-07-14 21:02:27 +0300

    Update Disemvowel_Trolls.js

--- learnJS/part_1/chap_4/codewars/Disemvowel_Trolls.js
+++ learnJS/part_1/chap_4/codewars/Disemvowel_Trolls.js
@@ -1,11 +1,11 @@
 // https://www.codewars.com/kata/52fba66badcd10859f00097e/train/javascript
 
-function disemvowel(str) {
-  var vowels = ['a', 'e', 'i', 'o', 'u'];
+function disemVowel(str) {
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
 return str
   .split('') // массив из букв
   .filter(el => vowels.indexOf(el.toLowerCase()) == -1) // убираем гласные
   .join(''); // соединяем обратно в строку
 }
 
-disemvowel("This website is for losers LOL!"); // "Ths wbst s fr lsrs LL!"
+disemVowel("This website is for losers LOL!"); // "Ths wbst s fr lsrs LL!"
