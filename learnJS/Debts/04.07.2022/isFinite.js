ffd64ee699615d6666505e8dc213bb6060b929dd
Igor <igorsmlff@gmail.com>
2022-07-14 12:46:48 +0300

    feat: debts

--- learnJS/Debts/04.07.2022/isFinite.js
+++ learnJS/Debts/04.07.2022/isFinite.js
@@ -20,3 +20,19 @@ isFinite("Сорок") // false
 isFinite(50) // true
 isFinite("101") // true
 isFinite("10e3") // true
+
+// Number.isNaN vs isNaN. Метод Number.isNaN() определяет, является ли переданное значение NaN. Это более надёжная версия оригинальной глобальной функции isNaN().
+/// При использовании глобальной функции isNaN() это всё будет true
+Number.isNaN('NaN');      // false
+Number.isNaN(undefined);  // false
+Number.isNaN({});         // false
+Number.isNaN('blabla');   // false
+
+/// А это всё в любом случае будет false
+Number.isNaN(true);
+Number.isNaN(null);
+Number.isNaN(37);
+Number.isNaN('37');
+Number.isNaN('37.37');
+Number.isNaN('');
+Number.isNaN(' ');
