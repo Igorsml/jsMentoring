6b7572aa262666f01bdf695607de104eb115465a
Igor Smolyanko <75610081+Igorsml@users.noreply.github.com>
2022-07-22 19:56:53 +0300

    Update 5_kyu_String incrementer.js

--- learnJS/part_1/chap_4/codewars/30_tasks/5_kyu_String incrementer.js
+++ learnJS/part_1/chap_4/codewars/30_tasks/5_kyu_String incrementer.js
@@ -1,12 +1,20 @@
-// https://www.codewars.com/kata/54a91a4883a7de5d7800009c/train/javascript
 "use strict";
 
-function incrementString(str) {
-  let sum = 0;
-  for (let i = 0; i < str.length; i++) {
-    sum += Number.isNaN(Number(str[i])) ? 0 : str[i];
+function incrementString(strng) {
+  const arrOfDigits = Array.from(strng, Number);
+  const arrOfStrings = Array.from(arrOfDigitsstrng, String);
+  console.log(arrOfDigits);
+  console.log(arrOfStrings);
+  let result = [];
+
+  for (let i = 0; i < arrOfDigits.length; i++) {
+    if (arrOfDigits[i] === NaN) {
+      result.push(arrOfDigits.splice(i, 1));
+    } else if (arrOfDigits[i] !== NaN) {
+      result.push(arrOfDigits[i] + 1);
+    }
+    return arrOfStrings + result.join("");
   }
-  return sum;
 }
 
 incrementString("foobar000"); // "foobar001"
@@ -17,7 +25,6 @@ incrementString("foobar099"); // "foobar100"
 incrementString(""); // "1"
 
 /*
-#1
 function incrementString(strng) {
   let strSplited = strng.split("");
   let strToNumber = +(strSplited.length[-1]);
@@ -32,24 +39,4 @@ function incrementString(strng) {
   } 
   return result;
 }
-
-#2
-function incrementString(strng) {
-  const arrOfDigits = Array.from(strng, Number);
-  console.log(arrOfDigits);
-  const arrOfStrings = Array.from(strng, String);
-  console.log(arrOfStrings);
-  let result = [];
-  
-  for (let i = 0; i < arrOfDigits.length; i++) {
-    if (arrOfDigits[i] === NaN) {
-      result.push(arrOfDigits[i]);
-      result.splice(arrOfDigits.length - 1, 1);
-    } else if (arrOfDigits[i] !== NaN) {
-      result.push(arrOfDigits[i] + 1);
-    }
-    return arrOfStrings.join("") + result.join("");
-  }
-}
-
 */
