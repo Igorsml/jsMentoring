2bedf85e7f31077a9e3727b192c862dacdf3eb9f
Igor <igorsmlff@gmail.com>
2022-07-26 17:59:49 +0300

    feat: task

---
+++ learnJS/part_1/chap_4/codewars/30_tasks/Completed/7_kyu_small_enough.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/57cc981a58da9e302a000214/train/javascript
+
+function smallEnough(a, limit) {
+  return a.every((number) => number <= limit);
+}
+
+//https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/every
--- learnJS/part_1/chap_4/codewars/30_tasks/Completed/For_myself.js
+++ learnJS/part_1/chap_4/codewars/30_tasks/Completed/For_myself.js
@@ -1,4 +1,4 @@
 const adaptiveNameTask = (str) => str.toLowerCase().replaceAll(" ", "_");
 
 adaptiveNameTask("5 kyu The Hashtag Generator"); // '5_kyu_the_hashtag_generator'
-console.log(adaptiveNameTask("5 kyu The Hashtag Generator"));
+console.log(adaptiveNameTask("7 kyu Small enough?"));
