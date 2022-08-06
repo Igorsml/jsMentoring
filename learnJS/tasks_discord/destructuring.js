06ba21701d9e43bef4274d43d0f242a3fbfbaa76
Igor <igorsmlff@gmail.com>
2022-08-06 18:18:23 +0300

    feat: destructuring

--- learnJS/tasks_discord/destructuring.js
+++ learnJS/tasks_discord/destructuring.js
@@ -31,6 +31,22 @@ obj[Symbol.iterator] = function () {
 const [firstPetName, secondPetName, ...rest] = obj;
 console.log(firstPetName, secondPetName, ...rest); // Miley Timmy Miriam
 
-/* Деструктрурировать массив как объект и получить не undefined значения. */
+/* Деструктрурировать массив как объект и получить не undefined значения. 
+..rest | TypeError: Found non-callable @@iterator */
 
-const arr = ["dogGirl:", "Miley"];
+const arr = ["Miley", "Timmy"];
+const fuckOffIamObj = Object.assign({}, arr);
+
+function catchMyValues(fuckOffIamObj) {
+  for (const value of Object.entries(fuckOffIamObj)) {
+    let result = ({ 0: firstPetName, 1: secondPetName } = fuckOffIamObj);
+  }
+  return console.log(firstPetName, secondPetName);
+}
+
+catchMyValues(fuckOffIamObj); // Miley Timmy
+
+// задать default
+const arr = ["Miley", "Timmy"];
+const obj = ({ firstPetName = "Abobus", secondPetName = "Abobusina" } = arr);
+console.log(firstPetName, secondPetName); // Abobus Abobusina
