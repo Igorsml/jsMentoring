b10313cd9b85b1fb1ac7c753e74e24fdde65109b
Igorsml <igorsmlff@gmail.com>
2022-08-15 01:43:27 +0300

    feat: fix folder

--- learnJS/part_1/chap_4-6/weather/script.js
+++ learnJS/part_1/chap_4-6/weather/script.js
@@ -1,4 +1,4 @@
-import moment from "moment";
+// import moment from "moment";
 ("use strict");
 //  JSON очень удобно использовать для получения данных в сети. Например, один из популярных сервисов прогноза погоды, Open Weather, может отдавать данные в JSON через API.
 let timezone = 3600;
