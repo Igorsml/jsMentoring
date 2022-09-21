484d340250a8d180a7aa891f07043af5758033ce
Igor <igorsmlff@gmail.com>
2022-09-21 12:50:40 +0300

    feat: classes

--- learnJS/part_1/chap_9/9.2_class-inheritance.js
+++ learnJS/part_1/chap_9/9.2_class-inheritance.js
@@ -41,3 +41,86 @@ cat.run("15km"); // 'Miriam running with speed 15km'
 cat.jumpHeigh(); // 'Miriam can jump heigh'
 cat.lickWater(); // 'Miriam can lick water'
 cat.lickWater(); // 'Miriam can lick wine'
+
+// если у расширяемого класса нет конструктора, он автоматически берется у наследуемого
+
+class Animal {
+  constructor(name) {
+    this.speed = 0;
+    this.name = name;
+  }
+
+  run(speed) {
+    this.speed = speed;
+    console.log(`${this.name} running with speed ${this.speed}`);
+  }
+
+  stop() {
+    this.speed = 0;
+    console.log(`${this.name} stopped `);
+  }
+
+  lickWater() {
+    console.log(`${this.name} can lick water`);
+  }
+}
+
+class Cat extends Animal {
+  // constructor(...args) {
+  //   super(...args);
+  // }
+  // если создадим свой, то вызов от наследуемого прекратится
+  constructor(name, age) {
+    super(name);
+    this.age = age;
+  }
+}
+
+let cat = new Cat("Miriam", 8);
+console.log(cat.name); // 'Miriam'
+console.log(cat.age); // 8
+
+// свойство [[HomeObject]]
+const food = {
+  name: "apple",
+  eat() {
+    // food.eat[[HomeObject]] == food
+    console.log(`Eat ${this.name}`);
+  },
+};
+
+let fruit = {
+  __proto__: food,
+  name: "orange",
+  eat() {
+    // fruit.eat[[HomeObject]] == fruit
+    super.eat();
+  },
+};
+
+let hasVitaminC = {
+  __proto__: fruit,
+  name: "kiwi",
+  eat() {
+    // hasVitaminC.eat.[[HomeObject]] == hasVitaminC
+    super.eat();
+  },
+};
+
+hasVitaminC.eat(); // Eat kiwi
+
+// nameFunction: function() and [[HomeObject]]
+const testLongHand = {
+  sleep: function () {
+    console.log("zZzZzz");
+  },
+};
+
+const igor = {
+  __proto__: testLongHand,
+  sleep: function () {
+    super.sleep();
+  },
+};
+
+igor.sleep(); // SyntaxError: 'super' keyword unexpected here
--- learnJS/part_1/chap_9/9.2_class-inheritance_ljTasks.js
+++ learnJS/part_1/chap_9/9.2_class-inheritance_ljTasks.js
@@ -1,1 +1,63 @@
 "use strict";
+
+// Task #1 Ошибка создания экземпляра класса
+// В коде ниже класс Rabbit наследует Animal.
+// К сожалению, объект класса Rabbit не создаётся. Что не так? Исправьте ошибку.
+
+class Animal {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+class Rabbit extends Animal {
+  constructor(name) {
+    this.name = name;
+    this.created = Date.now();
+  }
+}
+
+let rabbit = new Rabbit("Белый кролик"); // Error: this is not defined
+console.log(rabbit.name);
+
+// Task # 2
+// У нас есть класс Clock. Сейчас он выводит время каждую секунду
+
+class Clock {
+  constructor({ template }) {
+    this.template = template;
+  }
+
+  render() {
+    let date = new Date();
+
+    let hours = date.getHours();
+    if (hours < 10) hours = "0" + hours;
+
+    let mins = date.getMinutes();
+    if (mins < 10) mins = "0" + mins;
+
+    let secs = date.getSeconds();
+    if (secs < 10) secs = "0" + secs;
+
+    let output = this.template
+      .replace("h", hours)
+      .replace("m", mins)
+      .replace("s", secs);
+
+    console.log(output);
+  }
+
+  stop() {
+    clearInterval(this.timer);
+  }
+
+  start() {
+    this.render();
+    this.timer = setInterval(() => this.render(), 1000);
+  }
+}
+
+// Создайте новый класс ExtendedClock, который будет наследоваться от Clock и добавьте параметр precision – количество миллисекунд между «тиками». Установите значение в 1000 (1 секунда) по умолчанию.
+// Сохраните ваш код в файл extended-clock.js
+// Не изменяйте класс clock.js. Расширьте его.
