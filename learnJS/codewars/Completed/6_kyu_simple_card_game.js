e0e6d00ebef925c5e071f5207b3a4b3427b95082
Igor <igorsmlff@gmail.com>
2022-10-04 12:19:14 +0300

    feat: complete switch case

--- learnJS/codewars/Completed/6_kyu_simple_card_game.js
+++ learnJS/codewars/Completed/6_kyu_simple_card_game.js
@@ -2,10 +2,8 @@
 
 // избавиться от дублирования кода 8-24 и 28-44
 // переписать if’ы на switch, уведиться что работает,
-// потом переписать на объект ключ-значение, убедиться что работает
-
 function winner(deckSteve, deckJosh) {
-  const arrCards = [
+  const ranks = [
     "2",
     "3",
     "4",
@@ -20,62 +18,60 @@ function winner(deckSteve, deckJosh) {
     "K",
     "A",
   ];
-  let Steve = 0;
-  let Josh = 0;
-
-  switch (true) {
-    case deckSteve.indexOf("T"):
-    case deckJosh.indexOf("T"):
-      deckSteve.indexOf(10);
-      deckJosh.indexOf(10);
-      break;
-
-    case deckSteve.includes("J"):
-    case deckJosh.includes("J"):
-      deckSteve.indexOf(11);
-      deckJosh.indexOf(11);
-      break;
-
-    case deckSteve.includes("Q"):
-    case deckJosh.includes("Q"):
-      deckSteve.indexOf(12);
-      deckJosh.indexOf(12);
-      break;
-
-    case deckSteve.includes("K"):
-    case deckJosh.includes("K"):
-      deckSteve.indexOf(13);
-      deckJosh.indexOf(13);
-      break;
-
-    case deckSteve.includes("A"):
-    case deckJosh.includes("A"):
-      deckSteve.indexOf(14);
-      deckJosh.indexOf(14);
-      break;
-    default:
-      deckSteve, deckJosh;
-  }
-
-  console.log(deckSteve, deckJosh);
+  let Steve = 0,
+    Josh = 0;
 
   deckSteve.forEach((value, i) => {
     const value2 = deckJosh[i];
 
-    if (value > value2) {
-      Steve += 1;
-    }
-    if (value < value2) {
-      Josh += 1;
+    switch (true) {
+      case ranks.indexOf(value) > ranks.indexOf(value2):
+        Steve += 1;
+        break;
+
+      case ranks.indexOf(value2) > ranks.indexOf(value):
+        Josh += 1;
+        break;
     }
   });
 
-  if (Steve === Josh) return "Tie";
+  return Steve > Josh
+    ? `Steve wins ${Steve} to ${Josh}`
+    : Josh > Steve
+    ? `Josh wins ${Josh} to ${Steve}`
+    : "Tie";
+}
+
+console.log(winner(["T", "7", "8"], ["K", "5", "9"]));
+
+// потом переписать на объект ключ-значение, убедиться что работает
+function winner(deckSteve, deckJosh) {
+  const ranks = {
+    ...["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"],
+  };
+  const deckSteveObj = { ...deckSteve };
+  const deckJoshObj = { ...deckJosh };
+
+  let Steve = 0,
+    Josh = 0;
+
+  for (const value in deckSteveObj) {
+    if (deckSteveObj[value] > ranks[value]) {
+      Steve += 1;
+    }
+    if (deckSteveObj[value] < ranks[value]) {
+      Steve += 1;
+    }
+  }
 
   return Steve > Josh
     ? `Steve wins ${Steve} to ${Josh}`
-    : `Josh wins ${Josh} to ${Steve}`;
+    : Josh > Steve
+    ? `Josh wins ${Josh} to ${Steve}`
+    : "Tie";
 }
+
+console.log(winner(["T", "7", "8"], ["K", "5", "9"]));
 
 // previous variant
 function winner(deckSteve, deckJosh) {
@@ -139,3 +135,58 @@ function winner(deckSteve, deckJosh) {
     ? `Steve wins ${Steve} to ${Josh}`
     : `Josh wins ${Josh} to ${Steve}`;
 }
+
+// shit happens
+function winner(deckSteve, deckJosh) {
+  let Steve = 0;
+  let Josh = 0;
+
+  deckSteve.forEach((value, i) => {
+    const value2 = deckJosh[i];
+
+    switch (true) {
+      case value === "T":
+      case value2 === "T":
+        10;
+        break;
+      case value === "J":
+      case value2 === "J":
+        11;
+        break;
+
+      case value === "Q":
+      case value2 === "Q":
+        12;
+        break;
+
+      case value === "K":
+      case value2 === "K":
+        13;
+        break;
+
+      case value === "A":
+      case value2 === "A":
+        14;
+        break;
+
+      default:
+        value, value2;
+        console.log("value, value2:", value, value2);
+    }
+
+    if (value > value2) {
+      Steve += 1;
+    }
+    if (value < value2) {
+      Josh += 1;
+    }
+  });
+
+  if (Steve === Josh) return "Tie";
+
+  return Steve > Josh
+    ? `Steve wins ${Steve} to ${Josh}`
+    : `Josh wins ${Josh} to ${Steve}`;
+}
+
+console.log(winner(["T", "7", "8"], ["K", "5", "9"]));
