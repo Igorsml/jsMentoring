b01f533c34d1b2ad7c7192575f52fde0901b4e6c
Igor <igorsmlff@gmail.com>
2022-10-17 18:00:07 +0300

    feat: linked list

--- learnJS/Debts/byMentor/Maksimo/linked_list.js
+++ learnJS/Debts/byMentor/Maksimo/linked_list.js
@@ -22,14 +22,94 @@ let list2 = {
   previous: null,
   value: 1,
   next: {
-    // previous:
+    // previous: this.value,
     value: 2,
     tail: {
-      // previous:
+      // previous: this.value,
       value: 3,
       next: null,
     },
   },
 };
 
-console.log(list2);
+// Class linked list
+class LinkedList {
+  constructor() {
+    this.nodes = [];
+  }
+
+  get size() {
+    return this.nodes.length;
+  }
+
+  get head() {
+    return this.size ? this.nodes[0] : null;
+  }
+
+  get tail() {
+    return this.size ? this.nodes[this.size - 1] : null;
+  }
+
+  insertAt(index, value) {
+    const previousNode = this.nodes[index - 1] || null;
+    const nextNode = this.nodes[index] || null;
+    const node = { value, next: nextNode };
+
+    if (previousNode) previousNode.next = node;
+    this.nodes.splice(index, 0, node);
+  }
+
+  insertFirst(value) {
+    this.insertAt(0, value);
+  }
+
+  insertLast(value) {
+    this.insertAt(this.size, value);
+  }
+
+  getAt(index) {
+    return this.nodes[index];
+  }
+
+  removeAt(index) {
+    const previousNode = this.nodes[index - 1] || null;
+    const nextNode = this.nodes[index] || null;
+    if (previousNode) previousNode.next = nextNode;
+
+    return this.nodes.splice(index, 1);
+  }
+
+  clear() {
+    this.nodes = [];
+  }
+
+  reverse() {
+    this.nodes = this.nodes.reduce(
+      (acc, { value }) => [{ value, next: acc[0] || null }, ...acc],
+      []
+    );
+  }
+
+  *[Symbol.iterator]() {
+    yield* this.nodes;
+  }
+}
+
+const list3 = new LinkedList();
+
+list3.insertFirst(3);
+list3.insertFirst(2);
+list3.insertFirst(1);
+
+console.log(list3.size); // 3
+console.log(list3.head.value); // 1
+console.log(list3.head.next.value); // 2
+console.log(list3.tail.value); // 3
+
+console.log(list3.removeAt(1)); // [ { value: 2, next: { value: 3, next: null } } ]
+console.log(list3.getAt(1).value); // 3
+console.log(list3.head.next.value); // 2
+
+list3.reverse(); // [ { value: 3, next: [Object] }, { value: 1, next: null } ]
+list3.clear();
+console.log(list3.size); // 0
--- learnJS/codewars/temp.js
+++ learnJS/codewars/temp.js
@@ -1,50 +1,80 @@
-"use strict";
-
-// потом переписать на объект ключ-значение, убедиться что работает
-function winner(deckSteve, deckJosh) {
-  const ranks = [
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "T",
-    "J",
-    "Q",
-    "K",
-    "A",
-  ];
-
-  const ranksObj = { ...ranks };
-  const deckSteveObj = { ...deckSteve };
-  const deckJoshObj = { ...deckJosh };
-
-  let steve = 0,
-    josh = 0;
-
-  for (const value in deckSteveObj) {
-    if (
-      ranksObj.indexOf(deckSteveObj[value]) >
-      ranksObj.indexOf(deckJoshObj[value])
-    ) {
-      steve += 1;
-    }
-    if (
-      ranksObj.indexOf(deckSteveObj[value]) <
-      ranksObj.indexOf(deckJoshObj[value])
-    ) {
-      josh += 1;
-    }
+class LinkedList {
+  constructor() {
+    this.nodes = [];
   }
 
-  return steve > josh
-    ? `Steve wins ${steve} to ${josh}`
-    : josh > steve
-    ? `Josh wins ${josh} to ${steve}`
-    : "Tie";
+  get size() {
+    return this.nodes.length;
+  }
+
+  get head() {
+    return this.size ? this.nodes[0] : null;
+  }
+
+  get tail() {
+    return this.size ? this.nodes[this.size - 1] : null;
+  }
+
+  insertAt(index, value) {
+    const previousNode = this.nodes[index - 1] || null;
+    const nextNode = this.nodes[index] || null;
+    const node = { value, next: nextNode };
+
+    if (previousNode) previousNode.next = node;
+    this.nodes.splice(index, 0, node);
+  }
+
+  insertFirst(value) {
+    this.insertAt(0, value);
+  }
+
+  insertLast(value) {
+    this.insertAt(this.size, value);
+  }
+
+  getAt(index) {
+    return this.nodes[index];
+  }
+
+  removeAt(index) {
+    const previousNode = this.nodes[index - 1] || null;
+    const nextNode = this.nodes[index] || null;
+    if (previousNode) previousNode.next = nextNode;
+
+    return this.nodes.splice(index, 1);
+  }
+
+  clear() {
+    this.nodes = [];
+  }
+
+  reverse() {
+    this.nodes = this.nodes.reduce(
+      (acc, { value }) => [{ value, next: acc[0] || null }, ...acc],
+      []
+    );
+  }
+
+  *[Symbol.iterator]() {
+    yield* this.nodes;
+  }
 }
 
-console.log(winner(["T", "7", "8"], ["K", "5", "9"]));
+const list3 = new LinkedList();
+
+list3.insertFirst(3);
+list3.insertFirst(2);
+list3.insertFirst(1);
+
+console.log(list3.size); // 3
+console.log(list3.head.value); // 1
+console.log(list3.head.next.value); // 2
+console.log(list3.tail.value); // 3
+
+console.log(list3.removeAt(1)); // [ { value: 2, next: { value: 3, next: null } } ]
+console.log(list3.getAt(1).value); // 3
+console.log(list3.head.next.value); // 2
+
+list3.reverse(); // [ { value: 3, next: [Object] }, { value: 1, next: null } ]
+list3.clear();
+console.log(list3.size); // 0
