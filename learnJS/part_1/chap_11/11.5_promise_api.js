76b172d1dd5d8223fe88bc3ded6456b646d5f786
Igor <igorsmlff@gmail.com>
2022-12-08 16:03:51 +0300

    feat: Promise.all()

---
+++ learnJS/part_1/chap_11/11.5_promise_api.js
@@ -0,0 +1,72 @@
+"use strict";
+
+/* Promise.all(iterable),принимает итерируемую коллекцию промисов (чаще всего — массив) 
+и возвращает новый промис, который будет выполнен, когда будут выполнены все промисы, 
+переданные в виде перечисляемого аргумента, или отклонён, если хотя бы один 
+из переданных промисов завершится с ошибкой. */
+
+const promise1 = new Promise((resolve) => setTimeout(() => resolve(1), 5e3));
+const promise2 = new Promise((resolve) => setTimeout(() => resolve(2), 2e3));
+const promise3 = new Promise((resolve) => setTimeout(() => resolve(3), 1e3));
+
+// передадим массив из созданных промисов
+Promise.all([promise1, promise2, promise3]).then(
+  ([response1, response2, response3]) => {
+    console.log(response1, response2, response3);
+  }
+); // через 5 сек 1 2 3
+
+// если хотя бы один из промисов завершится с ошибкой, то остальные будут проигнорированы
+const promise4 = new Promise((resolve) => setTimeout(resolve(1), 5e3));
+const promise5 = new Promise((resolve, reject) =>
+  setTimeout(reject("error"), 2e3)
+);
+const promise6 = new Promise((resolve) => setTimeout(resolve(1), 1e3));
+
+Promise.all([promise4, promise5, promise6])
+  .then(([response4, response5, response6]) => {
+    console.log(response4, response5, response6);
+  })
+  .catch((err) => {
+    console.log(err);
+  }); // немедленно error
+
+/* Если в Promise.all передать не промис, он вернёт переданные не промисы 
+в массив результатов как есть (под капотом при этом произойдёт его 
+преобразование с помощью метода Promise.resolve()). */
+
+// Передаю в Promise.all() промис promise1, число number и объект obj:
+const promiseSoup = new Promise((resolve, reject) =>
+  setTimeout(() => resolve(1), 1e3)
+);
+const number = 42;
+const obj = { key: "anus" };
+
+Promise.all([promiseSoup, number, obj]).then(
+  ([response1, response2, response3]) => {
+    console.log(response1, response2, response3);
+  }
+); // 1 42 { key: 'anus' }
+
+/* Довольно частое использование — это преобразование массива с данными 
+в массив с промисами с помощью map(). В map() для каждого элемента 
+создаётся промис, а затем полученный массив передаётся в Promise.all(). 
+Это позволит дождаться выполнения всех промисов, а затем обработать результат. */
+
+// Получения данных нескольких персонажей из вселенной звёздных войн через запрос к API
+const peopleIds = [1, 2, 13];
+const arrayFetchUsers = peopleIds.map((user) =>
+  fetch(`https://swapi.dev/api/people/${user}`).then((response) =>
+    response.json()
+  )
+);
+
+Promise.all(arrayFetchUsers)
+  .then((responses) => {
+    responses.forEach((resp) => {
+      console.log(resp.name); // Luke Skywalker, C-3PO, Chewbacca
+    });
+  })
+  .catch((err) => console(err.message));
+//  сначала три запроса к API, с помощью Promise.all(), дождёмся их завершения
+// и парсинга ответа в JSON, а затем выведется имя персонажа для каждого.
