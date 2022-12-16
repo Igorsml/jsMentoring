76ef6aa88a7e3c2a68b0678a0a9b6afef65734a8
Igorsml <igorsmlff@gmail.com>
2022-12-17 01:18:00 +0300

    feat: link generator

---
+++ temp.js
@@ -0,0 +1,66 @@
+<script src="https://cdn.rawgit.com/adriancooney/console.image/c9e6d4fd/console.image.min.js"></script>;
+
+function generateScreenshot() {
+  const alphabet = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  const path = "https://prnt.sc/";
+  const randomNum = Math.floor(Math.random() * 1e4);
+  const shuffled = [...alphabet].sort(() => 0.5 - Math.random());
+  const char = shuffled.slice(0, 2).join("");
+
+  return window.open(`${path}${char}${randomNum}`).focus();
+}
+
+generateScreenshot();
