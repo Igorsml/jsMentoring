b4f78ab127000d0eb621921f9d0af0b837f74147
Igorsml <igorsmlff@gmail.com>
2022-12-29 00:13:32 +0300

    feat: continue work with 5 kyu

--- codewars/5_kyu_a_simple_music_encoder.js
+++ codewars/5_kyu_a_simple_music_encoder.js
@@ -1,36 +1,34 @@
+// https://www.codewars.com/kata/58db9545facc51e3db00000a
+
 function compress(arr) {
   let ranges = [];
-  let restart;
-  let render;
+  let prev;
+  let next;
+  let nextNext;
+  let sequencesOfThreeUp;
+  let sequencesOfThreeDown;
 
-  arr.forEach((number, index) => {
-    if (number === arr[index + 1] || number === arr[index - 1]) {
-    restart = arr[i];
-    render = restart;
+  for (let i = 0; i < arr.length; i++) {
+    prev = arr[i];
+    next = prev;
 
-    // [1, 2, 2, 2, 3]
-    if (arr[i] === arr[i + 1] || arr[i] === arr[i - 1]) {
-      render = arr[i + 1];
-      i++;
+    sequencesOfThreeUp =
+      arr[i + 2] - arr[i + 1] == 1 && arr[i + 1] - arr[i] == 1;
+    sequencesOfThreeDown =
+      arr[i + 2] - arr[i + 1] == -1 && arr[i + 1] - arr[i] == -1;
+    // example [7, 6, 5]
+    if (sequencesOfThreeUp || sequencesOfThreeDown) {
+      while (arr[i + 1] - arr[i] == 1 || arr[i + 1] - arr[i] == -1) {
+        next = arr[i + 1];
+        i++;
+      }
     }
-  } 
-}
-  ranges.push(restart == render ? `${restart}` : `${restart}*${render}`);
-
-  // for (let i = 0; i < arr.length; i++) {
-  //   restart = arr[i];
-  //   render = restart;
-
-  //   // example 3 - 2 == 1
-  //   while (arr[i + 1] - arr[i] == 1) {
-  //     render = arr[i + 1];
-  //     i++;
-  //   }
-  //   ranges.push(restart == render ? `${restart}` : `${restart} - ${render}`);
-  // }
+    ranges.push(prev == next ? `${prev}` : `${prev} - ${next}`);
+  }
 
   return ranges;
 }
 
-// console.log(compress([2, 3, 4, 5, 10, 18, 19, 20])); //  "2-5", "10", "18-20"
-console.log(compress([1, 2, 2, 2, 3, 5, 5, 5])); //  "1,2*3,3"
+console.log(compress([3, 4, 2, 3, 4, 5, 10, 18, 19, 20])); //  "2-5", "10", "18-20"
+console.log(compress([3, 4, 7, 6, 5])); //  "2-5", "10", "18-20"
+// console.log(compress([1, 2, 2, 2, 3, 5, 5, 5])); //  "1,2*3,3"
