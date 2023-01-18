18ee39ca21407c117814036cabe1e4f49a4a0a80
Igor <igorsmlff@gmail.com>
2023-01-18 18:00:39 +0300

    feat: prototype

---
+++ learnJS/part_2/chap_1/1.6_dom-attributes-and-properties/1.6_dom-attributes-and-properties.js
@@ -0,0 +1,12 @@
+// DOM свойства обычные объекы
+document.body.WeightLoss = () => {
+  setInterval(() => {
+    console.log("You loose 1 Kg");
+  }, 1e3);
+};
+
+document.body.WeightLoss();
+
+Element.prototype.Elemental = () => {
+  console.log("Water elemental pipi");
+};
