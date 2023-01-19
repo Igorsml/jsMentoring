649b71abb1f4564bea09d003df9d593bc94b9212
Igor <igorsmlff@gmail.com>
2023-01-19 12:59:43 +0300

    feat: join start working

--- temp.js
+++ temp.js
@@ -13,18 +13,42 @@
 // console.log("array:", array); // [1, 2, 3, 'end'];
 
 // 2 and more arguments
-Array.prototype.myPush = function (value, ...rest) {
-  let arr = this;
+// Array.prototype.myPush = function (value, ...rest) {
+//   let arr = this;
 
-  for (let i = 0; i < arguments.length; i++) {
-    arr[arr.length] = arguments[i];
+//   for (let i = 0; i < arguments.length; i++) {
+//     arr[arr.length] = arguments[i];
+//   }
+
+//   return arr.length;
+// };
+
+// const array = [1, 2, 3];
+// const length = array.myPush("end", "end2", "end3");
+
+// console.log("length:", length); // 6
+// console.log("array:", array); // [ 1, 2, 3, 'end', 'end2', 'end3' ];
+
+// join
+Array.prototype.myJoin = function (separator) {
+  if (this.length === 0) "";
+  let result = "";
+
+  for (let i = 0; i < this.length; i++) {
+    if (separator) {
+      result += this[i] + separator;
+    }
+    result += this[i] + ",";
   }
 
-  return arr.length;
+  result = result.slice(0, -1);
+  return result;
 };
 
-const array = [1, 2, 3];
-const length = array.myPush("end", "end2", "end3");
+const array = ["a", "b", "c"];
+console.log(array.myJoin()); // 'a,b,c'
+console.log(array.myJoin("")); // 'abc'
+console.log(array.myJoin("-")); // 'a-b-c'
 
-console.log("length:", length); // 6
-console.log("array:", array); // [ 1, 2, 3, 'end', 'end2', 'end3' ];
+// Does it mutate?
+console.log("array:", array); // ["a", "b", "c"]
