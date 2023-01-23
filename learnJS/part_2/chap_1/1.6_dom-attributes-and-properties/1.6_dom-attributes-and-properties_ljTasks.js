d9f3e2b4c64048577423c05a97474f07ac96ec40
Igor <igorsmlff@gmail.com>
2023-01-23 14:52:08 +0300

    feat: dom attributes

--- learnJS/part_2/chap_1/1.6_dom-attributes-and-properties/1.6_dom-attributes-and-properties_ljTasks.js
+++ learnJS/part_2/chap_1/1.6_dom-attributes-and-properties/1.6_dom-attributes-and-properties_ljTasks.js
@@ -1,8 +1,20 @@
 // Task #1. Напишите код для выбора элемента с атрибутом data-widget-name из документа и прочитайте его значение.
-console.log();
-console.log("data-widget-name:", document.body.dataset.widget);
 
-// task #2
-// добавление стиля для одной ссылки
-let link = document.querySelector("a");
-link.style.color = "orange";
+console.log("data-widget-name:", datasetTest.dataset.widgetName); // 'menu'
+// ответ LJ
+const elem = document.querySelector("[data-widget-name]");
+console.log(elem.getAttribute("data-widget-name"));
+console.log(elem.dataset.widgetName);
+
+/* task #2
+Сделайте все внешние ссылки оранжевыми, изменяя их свойство style.
+Ссылка является внешней, если:
+Её href содержит ://
+Но не начинается с http://internal.com.*/
+let link = document.querySelectorAll("a[href*='://']");
+
+for (const elem of link) {
+  if (!elem.href.startsWith("http://internal.com")) elem.style.color = "orange";
+}
+
+//https://bobbyhadz.com/blog/javascript-get-element-by-href-attribute
