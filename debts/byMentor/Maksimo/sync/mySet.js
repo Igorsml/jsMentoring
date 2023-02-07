28e4d3a3e448da4998fe9de1127842bf1988394c
Igor <igorsmlff@gmail.com>
2023-02-07 12:20:18 +0300

    feat: MySet done

--- debts/byMentor/Maksimo/sync/mySet.js
+++ debts/byMentor/Maksimo/sync/mySet.js
@@ -1,29 +1,53 @@
 class MySet {
-  constructor(...obj) {
-    this.mySet = Object.entries(obj);
+  constructor(obj) {
+    this.obj = { ...obj };
+    this.length = obj.length;
   }
 
   add() {
-    for (let arg of arguments) {
-      if (arg === null) continue;
+    Array.from(arguments).forEach((elem, i) => {
+      if (elem === null) return;
 
-      this.mySet[arg] = arguments[arg];
-    }
+      this.obj[arguments.length + i] = elem;
+      this.length++;
+    });
   }
 
   has() {
-    //
+    for (const arg of arguments) {
+      return Object.values(this.obj).indexOf(arg) > -1 ? true : false;
+    }
   }
 
   delete() {
-    //
+    for (const arg of arguments) {
+      if (Object.values(this.obj).indexOf(arg) > -1) {
+        delete this.obj[arg];
+        this.length--;
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+
+  size() {
+    return this.length;
   }
 }
 
-let set = new MySet([1, 2]);
+let set = new MySet([0, 1]);
 
-console.log(set.add(3, 4, 5)); //
-console.log(set);
-// console.log(set.has(3)); //
-// console.log(set.delete(1)); //
-// console.log(set.size);
+console.log(set.add(null));
+console.log(set); // MySet { obj: { '0': 0, '1': 1 }, length: 2 }
+
+console.log(set.add(2, 3));
+console.log(set); // MySet { obj: { '0': 0, '1': 1, '2': 2, '3': 3 }, length: 4 }
+
+console.log(set.has(2)); // true
+console.log(set.has(4)); // false
+
+console.log(set.delete(3)); // true
+console.log(set); // true, MySet { obj: { '0': 0, '1': 1, '2': 2 }, length: 3 }
+
+console.log(set.size()); // 3
