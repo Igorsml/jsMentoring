628024a9522fec1b3900f086ff0acc8b56ccabda
Igor <igorsmlff@gmail.com>
2023-02-22 16:59:13 +0300

    feat: cross-check fix

--- debts/byMentor/Maksimo/sync/mySet.js
+++ debts/byMentor/Maksimo/sync/mySet.js
@@ -1,29 +1,29 @@
 class MySet {
   constructor(obj) {
-    this.obj = { ...obj };
-    this.length = obj.length;
+    this._obj = { ...obj };
+    this.size = obj.length;
   }
 
   add() {
     Array.from(arguments).forEach((elem, i) => {
       if (elem === null) return;
 
-      this.obj[arguments.length + i] = elem;
-      this.length++;
+      this._obj[arguments.length + i] = elem;
+      this._length++;
     });
   }
 
   has() {
     for (const arg of arguments) {
-      return Object.values(this.obj).indexOf(arg) > -1 ? true : false;
+      return Object.values(this._obj).indexOf(arg) > -1 ? true : false;
     }
   }
 
   delete() {
     for (const arg of arguments) {
-      if (Object.values(this.obj).indexOf(arg) > -1) {
-        delete this.obj[arg];
-        this.length--;
+      if (Object.values(this._obj).indexOf(arg) > -1) {
+        delete this._obj[arg];
+        this._length--;
         return true;
       } else {
         return false;
@@ -31,23 +31,30 @@ class MySet {
     }
   }
 
-  size() {
-    return this.length;
+  clear() {
+    this._obj = null;
+    return undefined;
   }
 }
 
 let set = new MySet([0, 1]);
 
-console.log(set.add(null));
-console.log(set); // MySet { obj: { '0': 0, '1': 1 }, length: 2 }
+// Cat cross-check:
+console.log(set.obj); // undefined
+console.log(set.size); // 2
 
-console.log(set.add(2, 3));
-console.log(set); // MySet { obj: { '0': 0, '1': 1, '2': 2, '3': 3 }, length: 4 }
+// end cross-check
 
-console.log(set.has(2)); // true
-console.log(set.has(4)); // false
+// console.log(set.add(null));
+// console.log(set); // MySet { obj: { '0': 0, '1': 1 }, length: 2 }
 
-console.log(set.delete(3)); // true
-console.log(set); // true, MySet { obj: { '0': 0, '1': 1, '2': 2 }, length: 3 }
+// console.log(set.add(2, 3));
+// console.log(set); // MySet { obj: { '0': 0, '1': 1, '2': 2, '3': 3 }, length: 4 }
 
-console.log(set.size()); // 3
+// console.log(set.has(2)); // true
+// console.log(set.has(4)); // false
+
+// console.log(set.delete(3)); // true
+// console.log(set); // true, MySet { obj: { '0': 0, '1': 1, '2': 2 }, length: 3 }
+
+// console.log(set.size); // 3
